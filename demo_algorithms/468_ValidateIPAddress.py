489acfab138d096f67988ad204c14e29883c624a
JianxiaoLuo <jluo074@uottawa.ca>
2020-06-17 14:29:03 -0400

    Update 468_ValidateIPAddress.py

--- demo_algorithms/468_ValidateIPAddress.py
+++ demo_algorithms/468_ValidateIPAddress.py
@@ -12,8 +12,18 @@ class Solution:
             return "IPv4"
         return "IPv6" if self.patten_IPv6.match(IP) else "Neither" 
 
+    
+# Use ipaddress package
+from ipaddress import ip_address, IPv6Address
+class Solution:
+    def validIPAddress(self, IP: str) -> str:
+        try:
+            return "IPv6" if type(ip_address(IP)) is IPv6Address else "IPv4"
+        except ValueError:
+            return "Neithe
 
 
+# My solution
 class Solution:
     def validIPAddress(self, IP: str) -> str:
         if '.' in IP:

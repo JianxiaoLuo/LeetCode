2fa2a699cf18c83a8229afdeb56f5a9d5a5ab59b
JianxiaoLuo <jluo074@uottawa.ca>
2020-07-21 12:41:30 -0400

    Update 79_WordSearch.py

--- demo_algorithms/79_WordSearch.py
+++ demo_algorithms/79_WordSearch.py
@@ -1,3 +1,37 @@
+# My solution (81%)
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        
+        def dfs(i, j, index):
+            if i >= 0 and i < height and j >= 0 and j < width:  
+                if board[i][j] == word[index]:
+                    if index == w_len-1:
+                        self.flag = True
+                        return True
+                    tmp = board[i][j]
+                    board[i][j] = '$'
+                    if dfs(i-1, j, index+1) or dfs(i+1, j, index+1) or dfs(i, j-1, index+1) or dfs(i, j+1, index+1):         
+                        return True
+                    board[i][j] = tmp
+            if self.flag:
+                return 
+            return 
+               
+        
+        width, height, w_len = len(board[0]), len(board), len(word)
+        self.flag = False
+        
+        for i in range(height):
+            for j in range(width):
+                dfs(i, j, 0)
+                if self.flag: return True
+                else: continue
+        return False
+    
+     
+
+
+
 # My solution, cannot work
 
 class Solution:
